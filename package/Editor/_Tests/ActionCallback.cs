a5a83b6c6c575b8656b3a2070aa53bec78685a64
Marcel Wiessler <marcel@gaisterhand.de>
2021-04-01 09:57:20 +0200

    action callback test

---
+++ package/Editor/_Tests/ActionCallback.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Needle
+{
+	public static class ActionCallback
+	{
+		public static string Action(this string message, string key, Action act)
+		{
+			if (!callbacks.ContainsKey(key))
+				callbacks.Add(key, act);
+			else callbacks[key] = act;
+			
+			return message.LinkTo(prefix + key);
+		}
+
+		private const string prefix = "Action=";
+		private static readonly Dictionary<string, Action> callbacks = new Dictionary<string, Action>();
+		
+		[HyperlinkCallback]
+		private static void Callback(string path)
+		{
+			if (path.StartsWith(prefix))
+			{
+				var key = path.Substring(prefix.Length);
+				if (callbacks.TryGetValue(key, out var act))
+				{
+					act?.Invoke();
+				}
+			}
+		}
+	}
+}
---
+++ package/Editor/_Tests/FrameObjects.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+
+namespace Needle
+{
+	public static class FrameObjects
+	{
+		private const string framePrefix = "frame=";
+		
+		public static string Frame(this string message, Transform target)
+		{
+			return message.LinkTo(framePrefix + target.GetInstanceID());
+		}
+		
+		public static string Frame(this string message, IEnumerable<Transform> targets)
+		{
+			return message.LinkTo(framePrefix + string.Join(",", targets.Select(t => t.GetInstanceID().ToString())));
+		}
+		
+		[HyperlinkCallback]
+		private static void Callback(string path)
+		{
+			if (!path.StartsWith(framePrefix)) return;
+			
+			var sub = path.Substring(framePrefix.Length);
+			var sep = sub.Split(',');
+			var list = new List<Transform>();
+			foreach (var str in sep)
+			{
+				if (int.TryParse(str, out var id))
+				{
+					var obj = EditorUtility.InstanceIDToObject(id) as Transform;
+					if (obj)
+					{
+						list.Add(obj);
+					}
+				}
+			}
+
+			// ReSharper disable once CoVariantArrayConversion
+			Selection.objects = list.Select(t => t.gameObject).ToArray();
+
+			var bounds = new Bounds();
+			for (var index = 0; index < list.Count; index++)
+			{
+				var obj = list[index];
+				if (obj.TryGetComponent(out Renderer rend))
+				{
+					if (bounds.extents == Vector3.zero)
+						bounds = rend.bounds;
+					else bounds.Encapsulate(rend.bounds);
+				}
+				else
+				{
+					var _bounds = new Bounds(obj.position, Vector3.one);
+					if (bounds.extents == Vector3.zero)
+						bounds = _bounds;
+					else bounds.Encapsulate(_bounds);
+				}
+			}
+			SceneView.lastActiveSceneView.Frame(bounds, false);
+		}
+	}
+}
